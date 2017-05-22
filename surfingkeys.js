7ab0d6f4300186070574748bcc36730402f56a3c
Edwin <edwinvrooij@gmail.com>
2017-05-22 08:53:20 +0200

    -

--- surfingkeys.js
+++ surfingkeys.js
@@ -1,4 +1,4 @@
-// --- General settings ---
+// ================ General settings ================
 // todo: make a 'cu' for q (image click), open in non-active new tab
 
 settings.hintAlign = "left";
@@ -8,36 +8,26 @@ Hints.characters = 'aoeuidhtnspyfgcrl'; // all Dvorak first and second row alpha
 
 
 
-// --- Own mappings ---
+// ================ Own mappings ================
 
 // VIM editor mappings
-aceVimMap('nh', '<Esc>', 'insert');
 aceVimMap('<Space>c', 'ciw', 'normal');
 aceVimMap('H', '0', 'normal');
 aceVimMap('L', '$', 'normal');
 
 // Open links
 map('u', 'f');
-unmap('f');
-
 map('U', 'af');
-unmap('af');
 
+map('gu', 'U');
 map('gu', 'gf');
-unmap('gf');
 
 map('cu', 'cf');
-unmap('cf');
-
 map('cc', 'p'); // 'paste' = open link from clipboard
 
 // More natural previous/back navigation
-unmap('>>');
-unmap('<<');
 map('>', ']]');
 map('<', '[[');
-unmap('[[');
-unmap(']]');
 
 // More confortable tab movement
 map('<', 'E');
@@ -48,16 +38,13 @@ map('K', 'e');
 
 // Tab history management
 map('h', 'S');
-unmap('S');
-
 map('l', 'D');
-unmap('D');
 
 // Search with space
-map('<Space>', '/');
+map('<Space><Space>', '/');
 
 
 
-// --- Exceptions ---
+// ================ Exceptions ================
 // todo: 9gag jk movement
 // todo; make shift+enter in t google for result instead of selected
