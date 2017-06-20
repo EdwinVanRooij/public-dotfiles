1eed1d37913359fc79acfcf9cc002077b51428eb
Edwin <edwinvrooij@gmail.com>
2017-06-20 08:23:23 +0200

    -

--- surfingkeys.js
+++ surfingkeys.js
@@ -6,6 +6,8 @@ settings.startToShowEmoji = 1;
 settings.scrollStepSize = 120; // faster 'jk' scrolling
 Hints.characters = 'aoeuidhtnspyfgcrl'; // all Dvorak first and second row alphabetic characters
 
+settings.nextLinkRegex = '/((hurrrdurrrr)+)/i';
+settings.prevLinkRegex = '/((hurrrdurrrr)+)/i';
 
 
 // ================ Own mappings ================
