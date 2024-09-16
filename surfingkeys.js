5d72e56a124dacce1072c05a2b9654c5f928d4d6
Edwin <edwinvrooij@gmail.com>
2024-09-16 09:49:52 +0200

    Update surfingkeys.js

--- surfingkeys.js
+++ surfingkeys.js
@@ -1,49 +1,26 @@
-const {
-    aceVimMap,
-    mapkey,
-    imap,
-    imapkey,
-    getClickableElements,
-    vmapkey,
-    map,
-    iunmap,
-    unmap,
-    unmapAllExcept,
-    vunmap,
-    cmap,
-    addSearchAlias,
-    removeSearchAlias,
-    tabOpenLink,
-    readText,
-    Clipboard,
-    Front,
-    Hints,
-    Visual,
-    RUNTIME
-} = api;
-
 // ================ General settings ================
 settings.hintAlign = "left";
 settings.startToShowEmoji = 1;
 settings.scrollStepSize = 120; // faster 'jk' scrolling
-Hints.characters = 'aoeuidhtnspyfgcrl'; // all Dvorak first and second row alphabetic characters
+//Hints.characters = 'aoeuidhtnspyfgcrl'; // all Dvorak first and second row alphabetic characters
+api.Hints.characters = 'aoeuidhtnspyfgcrl'; // all Dvorak first and second row alphabetic characters
 
-iunmap(":");
-unmap("/"); // quick search
-unmap('r');
+api.iunmap(":");
+api.unmap("/"); // quick search
+api.unmap('r');
 
 // ================ Own mappings ================
-map('u', 'f'); // open link
-map('U', 'gf'); // open link in new tab
+api.map('u', 'f'); // open link
+api.map('U', 'gf'); // open link in new tab
 
-map('p', 'cc'); // 'paste' = open link from clipboard
+api.map('p', 'cc'); // 'paste' = open link from clipboard
 
-map('J', 'd'); // fast scroll down
-map('K', 'e'); // fast scroll up
+api.map('J', 'd'); // fast scroll down
+api.map('K', 'e'); // fast scroll up
 
 // --- Tabs
-map('h', 'S'); // previous tab
-map('l', 'D'); // next tab
+api.map('h', 'S'); // previous tab
+api.map('l', 'D'); // next tab
 
-map('<', 'E'); // previous tab
-map('>', 'R'); // next tab
+api.map('<', 'E'); // previous tab
+api.map('>', 'R'); // next tab
